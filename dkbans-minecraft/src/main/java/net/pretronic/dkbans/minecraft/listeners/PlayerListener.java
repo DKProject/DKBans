b3429c97cddf85d28b025cc9a63259acee826954
Davide Wietlisbach <davide.wietlisbach@icloud.com>
2020-06-13 17:21:09 +0200

    Updated copyright

--- dkbans-minecraft/src/main/java/net/pretronic/dkbans/minecraft/listeners/PlayerListener.java
+++ dkbans-minecraft/src/main/java/net/pretronic/dkbans/minecraft/listeners/PlayerListener.java
@@ -1,13 +1,30 @@
+/*
+ * (C) Copyright 2020 The DKBans Project (Davide Wietlisbach & Philipp Elvin Friedhoff)
+ *
+ * @author Davide Wietlisbach
+ * @since 13.06.20, 17:19
+ *
+ * The DKBans Project is under the Apache License, version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 package net.pretronic.dkbans.minecraft.listeners;
 
-import net.md_5.bungee.api.chat.TextComponent;
 import net.pretronic.dkbans.api.DKBans;
 import net.pretronic.dkbans.api.filter.FilterAffiliationArea;
 import net.pretronic.dkbans.api.filter.FilterManager;
 import net.pretronic.dkbans.api.player.DKBansPlayer;
 import net.pretronic.dkbans.api.player.history.PlayerHistoryEntry;
 import net.pretronic.dkbans.api.player.history.PunishmentType;
-import net.pretronic.dkbans.api.template.punishment.PunishmentTemplateEntry;
 import net.pretronic.dkbans.minecraft.config.DKBansConfig;
 import net.pretronic.dkbans.minecraft.config.Messages;
 import net.pretronic.libraries.event.EventPriority;
