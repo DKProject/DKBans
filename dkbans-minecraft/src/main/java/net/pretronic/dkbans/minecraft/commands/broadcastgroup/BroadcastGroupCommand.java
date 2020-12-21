af36efbd724652533548c67b6841e53affa6967d
Fridious <philippfriedhoff@outlook.de>
2020-12-21 20:04:46 +0100

    Fixed info command execution in BroadcastGroupCommand

--- dkbans-minecraft/src/main/java/net/pretronic/dkbans/minecraft/commands/broadcastgroup/BroadcastGroupCommand.java
+++ dkbans-minecraft/src/main/java/net/pretronic/dkbans/minecraft/commands/broadcastgroup/BroadcastGroupCommand.java
@@ -70,7 +70,7 @@ public class BroadcastGroupCommand extends MainObjectCommand<BroadcastGroup> imp
     @Override
     public void commandNotFound(CommandSender commandSender, BroadcastGroup group, String command, String[] args) {
         if(group != null) {
-            infoCommand.execute(commandSender, args);
+            infoCommand.execute(commandSender, group, args);
         } else {
             listCommand.execute(commandSender, args);
         }
