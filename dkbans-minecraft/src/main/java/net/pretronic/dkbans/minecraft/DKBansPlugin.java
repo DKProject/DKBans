f394ca9f300ccc7a42ec543fb7700bc26315beea
Davide Wietlisbach <davide.wietlisbach@icloud.com>
2020-12-27 15:31:04 +0100

    Removed broadcast start

--- dkbans-minecraft/src/main/java/net/pretronic/dkbans/minecraft/DKBansPlugin.java
+++ dkbans-minecraft/src/main/java/net/pretronic/dkbans/minecraft/DKBansPlugin.java
@@ -197,7 +197,6 @@ public class DKBansPlugin extends MinecraftPlugin {
         McNative.getInstance().getScheduler().createTask(this)
                 .delay(5, TimeUnit.SECONDS)
                 .interval(1, TimeUnit.SECONDS)
-                .execute(new BroadcastTask().start())
-                .start();
+                .execute(new BroadcastTask().start());
     }
 }
